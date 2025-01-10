416e5467fae253457dab2889875e136bba28331f
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2025-01-10 18:15:48 +0530

    [vishal]Add: solved problem on Aliases

---
+++ Aliases/queries.sql
@@ -0,0 +1,11 @@
+Q1: Create an alias for the full name of a customer by concatenating their first and last names.
+Query: SELECT CONCAT(first_name, ' ', last_name) AS full_name FROM employees ;
+
+  full_name  
+-------------
+ Aman Kumar
+ Varun Kumar
+  
+  
+(4 rows)
+
